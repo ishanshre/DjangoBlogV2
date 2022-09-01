25b6b6fa25f14f7af0aae7d7e435c68c8898d9bc
Ishan Shrestha <ishan.shrestha356@gmail.com>
2022-09-01 11:57:18 +0545

    add search function

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import IndexListView, PostDetailView
+from .views import IndexListView, PostDetailView, SearchBlogView
 
 
 app_name = 'blog'
 urlpatterns = [
     path('', IndexListView.as_view(), name = 'index'),
     path('<int:year>/<int:month>/<int:day>/<slug:slug>/', PostDetailView.as_view(), name='post_detail'),
+    path('search/', SearchBlogView.as_view(), name='post_search'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.views.generic import (
     ListView,
     DetailView,
 )
+from django.db.models import Q
 from .models import Post
 # Create your views here.
 
@@ -26,3 +27,15 @@ class PostDetailView(DetailView):
     def get_queryset(self):
         #post = Post.published.get(publish__year=self.kwargs['year'], publish__month=self.kwargs['month'], publish__day=self.kwargs['day'], slug=self.kwargs['slug'])
         return Post.published.filter(publish__year=self.kwargs['year'], publish__month=self.kwargs['month'], publish__day=self.kwargs['day'], slug=self.kwargs['slug'])
+
+
+class SearchBlogView(ListView):
+    model = Post
+    context_object_name = 'post_list'
+    paginate_by = 10
+    template_name = 'blog/post_search.html'
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Post.published.filter(
+            Q(title__icontains=query)|Q(author__username = query)
+        )
